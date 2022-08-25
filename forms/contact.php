1d3670b5040273a1762d00c897105ab0212df17d
Decagon <decagon@Decagons-MacBook-Air-Usha.local>
2022-08-25 18:48:01 +0100

    contact form now working

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,41 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
-
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
- 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
-?>
+$from = 'Contact us at wisdomofgodcn.org <info@wisdomofgodcn.org>'; // an email address that will be in the From field of the email.
+$sendTo = 'Contact us at wisdomofgodcn.org <info@wisdomofgodcn.org>'; // an email address that will receive the email with the output of the form
+$subject = 'New message from contact form'; // subject of the email
+$fields = array('name' => 'Name', 'email' => 'Email', 'subject' => 'Subject', 'message' => 'Message'); // form field names and their translations. // array variable name => Text to appear in the email
+$okMessage = 'Contact form successfully submitted. We will get back to you soon!'; // message that will be displayed when everything is OK :)
+$errorMessage = 'Error while submitting the form. Please try again later'; // If something goes wrong, we will display this message.
+error_reporting(E_ALL & ~E_NOTICE);
+try
+{
+    if(count($_POST) == 0) throw new \Exception('Form is empty');           
+    $emailText = "New message Wisdomofgodcn form\n=============================\n";
+    foreach ($_POST as $key => $value) {
+        // If the field exists in the $fields array, include it in the email 
+        if (isset($fields[$key])) {
+            $emailText .= "$fields[$key]: $value\n";
+        }
+    }
+    // All the necessary headers for the email.
+    $headers = array('Content-Type: text/plain; charset="UTF-8";',
+        'From: ' . $from,
+        'Reply-To: ' . $_POST['email'],
+        'Return-Path: ' . $from,
+    );
+    mail($sendTo, $subject, $emailText, implode("\n", $headers)); // Send email
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+}
+catch (\Exception $e)
+{
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+}
+// if requested by AJAX request return JSON response
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
+    header('Content-Type: application/json');
+    echo $encoded;
+}
+// else just display the message
+else {
+    echo $responseArray['message'];
+}
